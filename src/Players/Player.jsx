e2965d9951af5277eddd7af9c49e6c97bfe6a3f9
Aminul islam <aminurrohman98@gmail.com>
2026-03-24 23:23:38 +0000

    added header for cardContainer

--- src/Players/Player.jsx
+++ src/Players/Player.jsx
@@ -5,13 +5,26 @@ const Player = ({ playerRes }) => {
   const players = use(playerRes);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-6 container mx-auto mt-10">
-      {players.map((player) => (
-        <AvailablePlayer
-          key={player.playerId}
-          player={player}
-        ></AvailablePlayer>
-      ))}
+    <div className="container mx-auto mt-10">
+      <div className="flex justify-between items-center mb-5">
+        <h1 className="text-2xl font-semibold">Available Players</h1>
+        <div className="flex flex-row  font-bold">
+          <button className="py-2 px-7 bg-lime-400 border-2  border-gray-300 rounded-l-2xl hover:bg-gray-300 ">
+            Available
+          </button>
+          <button className="py-2 px-7 border-gray-300 border-2  rounded-r-2xl hover:bg-gray-300">
+            Selected
+          </button>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-6 ">
+        {players.map((player) => (
+          <AvailablePlayer
+            key={player.playerId}
+            player={player}
+          ></AvailablePlayer>
+        ))}
+      </div>
     </div>
   );
 };
