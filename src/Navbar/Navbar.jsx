7f553d36c2b3b79433154ffd7b0e3b22dc0d8b20
Aminul islam <aminurrohman98@gmail.com>
2026-03-24 23:04:18 +0000

    made the page responsive

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -4,15 +4,17 @@ import CoinImage from "../assets/coin.png";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="flex-none">
-        <button className="flex justify-between items-center font-semibold gap-2 px-4">
-          0 Coin
-          <img src={CoinImage} alt="coin-image" />
-        </button>
+    <div className="shadow mb-10">
+      <div className="navbar bg-base-100  container mx-auto  ">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <button className="flex justify-between items-center font-semibold gap-2 px-4">
+            0 Coin
+            <img src={CoinImage} alt="coin-image" />
+          </button>
+        </div>
       </div>
     </div>
   );
