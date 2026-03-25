7bd7cd51cdafe244a8d9959ae3e3412b351cdf5c
Aminul islam <aminurrohman98@gmail.com>
2026-03-25 13:17:11 +0000

    made toggle button for available section and selected player section

--- src/Players/Player.jsx
+++ src/Players/Player.jsx
@@ -1,23 +1,40 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import AvailablePlayer from "./AvailablePlayer";
+import SelectedPlayer from "./SelectedPlayer";
 
 const Player = ({ playerRes }) => {
   const players = use(playerRes);
+  const [available, setAvailable] = useState("available");
 
   return (
-    <div className="container mx-auto mt-10">
+    <div className="max-w-7xl mx-auto mt-10">
       <div className="flex justify-between items-center mb-5">
-        <h1 className="text-2xl font-semibold">Available Players</h1>
+        {available === "available" ? (
+          <h1 className="text-2xl font-semibold">Available Players</h1>
+        ) : (
+          <h1 className="text-2xl font-semibold">Selected Players (2/3)</h1>
+        )}
+
         <div className="flex flex-row  font-bold">
-          <button className="py-2 px-7 bg-lime-400 border-2  border-gray-300 rounded-l-2xl hover:bg-gray-300 ">
+          <button
+            onClick={() => {
+              setAvailable("available");
+            }}
+            className={`py-2 px-5 ${available === "available" ? " bg-[#E7FE29]" : "bg-white"} cursor-pointer border  border-gray-300 rounded-r-none rounded-l-2xl`}
+          >
             Available
           </button>
-          <button className="py-2 px-7 border-gray-300 border-2  rounded-r-2xl hover:bg-gray-300">
+          <button
+            onClick={() => {
+              setAvailable("selected");
+            }}
+            className={`py-2 px-5  ${available === "selected" ? " bg-[#E7FE29]" : "bg-white"} cursor-pointer border border-gray-300   rounded-r-2xl rounded-l-none `}
+          >
             Selected
           </button>
         </div>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-6 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-6 ">
         {players.map((player) => (
           <AvailablePlayer
             key={player.playerId}
---
+++ src/Players/SelectedPlayer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SelectedPlayer = () => {
+  return (
+    <div>
+      <p>Selected Player on active</p>
+    </div>
+  );
+};
+
+export default SelectedPlayer;
